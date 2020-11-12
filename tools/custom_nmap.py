78ea9dbad8c5a8692b05126da7621415239ea15a
garymalaysia <gtsai70@gmail.com>
2020-11-11 22:57:28 -0500

    Revert "removed old code"

--- tools/custom_nmap.py
+++ tools/custom_nmap.py
@@ -9,16 +9,31 @@ import sys
 # json_in_str  = json.loads(json_in_dict)
 
 buffer = []
-
-def josh_idea(ip):
+def scan_all_ports(host): # perfrom all port scan
     nmap = nmap3.NmapScanTechniques()
-    json_output  = nmap.nmap_syn_scan(target=ip, args=" -A -v -p-")
-    for each_ in json_output:
-        for dictionary in json_output.get(each_):
-            if type(dictionary) is dict:
-                print (dictionary.get("service"))
+    json_output  = nmap.nmap_syn_scan(target=host, args="-Pn -p 1-65535")
+    json_in_dict = json.dumps(json_output, indent=4)
+    json_in_str  = json.loads(json_in_dict)
+    for page in json_in_str:
+       for each in json_in_str.get(page):
+            if type(each) is dict:
+                if each['state'] == 'open':
+                    buffer.append(each['portid'])
 
+    return buffer
+
+
+def scan_all_open_ports(ports:list):
+    nmap = nmap3.NmapHostDiscovery()
+    #print(listToString(ports))
+    json_output  = nmap.nmap_portscan_only(target=sys.argv[1], args="-sV -sT -A -p %s" % (listToString(ports)))
+    json_in_dict = json.dumps(json_output, indent=4)
+    print (json_in_dict)
+
+
+def listToString(s):
+    str1 = ","
+    return (str1.join(s))
 
 if __name__ == "__main__":  
-    print(josh_idea(sys.argv[1]))
-
+    scan_all_open_ports(scan_all_ports(sys.argv[1]))
